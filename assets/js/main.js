f89a183c56a48f07f55fb54a26a2476dad1a4e26
dianix <dianavazquez.dcg@gmail.com>
2017-07-13 16:26:13 -0500

    se obtienen los datos de los planetas

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,22 +16,76 @@ getJSON("data/earth-like-results.json")
     .then(function (planetas) {
         planetas.results.forEach(function (planeta) {
             (getJSON(planeta).then(function (datos) {
-                //crearTarjeta(datos);
-                console.log(datos);
+                crearTarjeta(datos);
+                //console.log(datos);
             }))
         })
     })
 
-var crearTarjeta = function (datos) {
+
+var crearTarjeta = function (datos) {    
+    // obteniendo datos a usar
+    var nombre = datos.pl_name;
+    var fechaDesc = datos.pl_disc;
+    var pOrbita = datos.pl_orbper;
+    var masInfo = datos.pl_edelink;
+    console.log(pOrbita)
+
+    // llamando contenedor en html
+    var contenedorPlanetas = document.getElementById("planetas-datos");
+
+    // creando elementos para dom
+    var contTarjeta = document.createElement("div");
+    contTarjeta.className = "col s12 m6";
+    var tarjeta = document.createElement("div");
+    tarjeta.className = "card";
+
+    // creando sección imagen y nombre de planeta
+    var contImagen = document.createElement("div");
+    contImagen.className = "card-image";
+    var imagen = document.createElement("img");
+    imagen.src = "static/img/planeta.jpg";
+    var nombrePlaneta = document.createElement("span");
+    nombrePlaneta.className = "card-title";
+    nombrePlaneta.innerText = nombre;
+    //guardando elementos de sección imagen en su contenedor
+    contImagen.appendChild(imagen);
+    contImagen.appendChild(nombrePlaneta);
+
+    // creando sección de datos de planeta
+    var contDatos = document.createElement("div");
+    contDatos.className = "card-content";
+    var datoFecha = document.createElement("h6");
+    datoFecha.innerText = "Descubierto en : " + fechaDesc;
+    var datoOrbita = document.createElement("h6");
+    datoOrbita.innerText = "Periodo de órbita: " + pOrbita;
+    //guardando datos de planeta en su contenedor
+    contDatos.appendChild(datoFecha);
+    contDatos.appendChild(datoOrbita);
     
-    var tarjeta = document.createElement();
-    var fila = $("<div class='row center-align'/>")
-    $(pokemons).each(function (i, pokemon) {
-        var cajaPokemon = $("<div class='card col s6 m3 hoverable'/>");
-        var pokeFoto = $("<img class='responsive-img'/>").attr("src", pokeimagenes[i].imagen);
-        var nombre = $("<a class='datos'/>").text(pokemon.name).attr("data-info", pokemon.url).attr('href', "#modal1");
-        cajaPokemon.append(pokeFoto).append(nombre);
-        fila.append(cajaPokemon);
-        contenedorPokemones.append(fila)
-    });
-};
+    //creando sección enlace más info
+    var contEnlace = document.createElement("div");
+    contEnlace.className = "card-action";
+    var enlace = document.createElement("a");
+    enlace.setAttribute("href", masInfo);
+    enlace.innerText = "MÁS INFORMACIÓN";
+    console.log(enlace)
+    //guardando enlace en su contenedor
+    contEnlace.appendChild(enlace);
+    
+    //guardando secciones en tarjeta
+    tarjeta.appendChild(contImagen);
+    tarjeta.appendChild(contDatos);
+    tarjeta.appendChild(contEnlace);
+    console.log(tarjeta)
+    
+    //guardando tarjeta en contenedor con columnas responsive
+    contTarjeta.appendChild(tarjeta);
+    
+    //enviando tarjeta a html
+    contenedorPlanetas.appendChild(contTarjeta);
+}
+
+
+
+
