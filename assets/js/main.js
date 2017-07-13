30f88103683080b5fc173a23dab31dbb9255e977
dianix <dianavazquez.dcg@gmail.com>
2017-07-13 14:39:35 -0500

    obteniendo en consola datos de cada planeta

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,9 +13,25 @@ function getJSON(url) {
 };
 
 getJSON("data/earth-like-results.json")
-    .then(function (mensaje) {
-        return (getJSON(mensaje.results[0]))
+    .then(function (planetas) {
+        planetas.results.forEach(function (planeta) {
+            (getJSON(planeta).then(function (datos) {
+                //crearTarjeta(datos);
+                console.log(datos);
+            }))
+        })
     })
-    .then(function (resultado) {
-        console.log(resultado)
+
+var crearTarjeta = function (datos) {
+    
+    var tarjeta = document.createElement();
+    var fila = $("<div class='row center-align'/>")
+    $(pokemons).each(function (i, pokemon) {
+        var cajaPokemon = $("<div class='card col s6 m3 hoverable'/>");
+        var pokeFoto = $("<img class='responsive-img'/>").attr("src", pokeimagenes[i].imagen);
+        var nombre = $("<a class='datos'/>").text(pokemon.name).attr("data-info", pokemon.url).attr('href', "#modal1");
+        cajaPokemon.append(pokeFoto).append(nombre);
+        fila.append(cajaPokemon);
+        contenedorPokemones.append(fila)
     });
+};
